03516219cd8ba241ae0c3efca4eb98914f1313f5
Vitor Soares <pereiravitor1218@gmail.com>
2022-11-16 22:00:06 -0300

    feat: service > createUsers

---
+++ server/src/service/users/createUser.ts
@@ -0,0 +1,35 @@
+import crypto from 'crypto';
+import prisma from '../../client';
+
+export async function createUsers(username: string, password: string) {
+  if (!username || !password) {
+    return { code: 400, message: 'usuário ou senha em branco' };
+  }
+  if (username.length < 3) {
+    return { code: 400, message: 'usuário deve conter ao menos 3 caracteres' };
+  }
+  if (password.length < 8) {
+    return { code: 400, message: 'senha deve conter ao menos 8 caracteres' };
+  }
+  const existingUser = await prisma.users.findUnique({
+    where: {
+      username,
+    },
+  });
+  if (existingUser) {
+    return { code: 400, message: 'usuário já existe' };
+  }
+  const passHash = crypto.createHash('md5').update(password).digest('hex');
+  await prisma.users.create({
+    data: {
+      username,
+      password: passHash,
+      Account: {
+        create: {
+          balance: 100,
+        },
+      }
+    },
+  });
+  return { code: 201, message: 'usuário criado com sucesso' };
+}
