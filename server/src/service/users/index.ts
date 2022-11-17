1617210c52c02feeea21600d78c7726da7d5f20a
Vitor Soares <pereiravitor1218@gmail.com>
2022-11-16 22:01:42 -0300

    feat: index para importações de serviceUsers

---
+++ server/src/service/users/index.ts
@@ -0,0 +1,6 @@
+import { createUsers } from './createUser';
+import { getUserById } from './getUserById';
+import { getUsers } from './getUsers';
+import { login } from './login';
+
+export { createUsers, login, getUsers, getUserById };
