5d8fa6f46e69b54f66ef10546543912727ca0661
Vitor Soares <pereiravitor1218@gmail.com>
2022-11-16 22:00:28 -0300

    feat: service > login

---
+++ server/src/service/users/login.ts
@@ -0,0 +1,23 @@
+import crypto from 'crypto';
+import prisma from '../../client';
+import { createToken } from '../../helpers/createToken';
+
+export async function login(username: string, password: string) {
+  if (!username || !password) {
+    return { code: 400, message: 'usuário ou senha em branco' };
+  }
+  const passHash = crypto.createHash('md5').update(password).digest('hex');
+  const user = await prisma.users.findUnique({
+    where: {
+      username,
+    },
+  });
+  if (!user) {
+    return { code: 401, message: 'usuário não encontrado' };
+  }
+  if (user.password !== passHash) {
+    return { code: 401, message: 'senha incorreta' };
+  }
+  const token = createToken({ username, id: user.id });
+  return { code: 200, data: { username, id: user.id, token } };
+}
