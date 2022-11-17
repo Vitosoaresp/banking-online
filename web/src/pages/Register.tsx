53dd476a0ae858e9b14b4bf07e94d467c6fea707
Vitor Soares <pereiravitor1218@gmail.com>
2022-11-17 20:41:34 -0300

    feat[Register]: icones para input e ver a senha digitada

--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { Eye, EyeSlash, Lock, User } from 'phosphor-react';
+import { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 
 export function Register() {
@@ -8,13 +9,21 @@ export function Register() {
   const [password, setPassword] = useState('');
   const [isInvalidPassword, setIsInvalidPassword] = useState(false);
   const [isInvalidUsername, setIsInvalidUsername] = useState(false);
+  const [visibledPassword, setVisibledPassword] = useState(false);
 
   const regexPass = /^(?=.*\d)(?=.*[A-Z])[0-9a-zA-Z$*&@#]{8,}$/
 
+  useEffect(() => {
+    if (isInvalidPassword) {
+      setIsInvalidPassword(false);
+    }
+    if (isInvalidUsername) {
+      setIsInvalidUsername(false);
+    }
+  }, [username, password]);
+
   async function habdleSubmitForm(event: React.FormEvent) {
     event.preventDefault();
-    setIsInvalidPassword(false)
-    setIsInvalidUsername(false)
     if (!regexPass.test(password)) {
       setIsInvalidPassword(true);
     } else {
@@ -23,8 +32,8 @@ export function Register() {
         password,
       }).then(() => {
         history.push('/login');
-      }).catch(error => {
-        console.log(error);
+        alert('Usuário cadastrado com sucesso!');
+      }).catch(() => {
         setIsInvalidUsername(true);
       });
     }
@@ -32,34 +41,44 @@ export function Register() {
 
   return (
     <div className='flex w-full md:px-40 px-10 h-screen justify-center items-center flex-col bg-bg-animation bg-cover'>
-      <h1 className='text-white font-bold pb-10'>Crie uma conta grátis!</h1>
+      <h1 className='text-white font-bold pb-10 md:text-3xl text-xl'>Crie uma conta grátis!</h1>
       <form onSubmit={habdleSubmitForm} className='flex flex-col gap-5 md:w-[300px]'>
-          <label htmlFor="username" className='text-white flex flex-col w-full'>
-            Usuário
-            <input
-              type="text"
-              className='p-2 rounded-md placeholder-gray-900 placeholder:opacity-60 text-black'
-              name="username"
-              required
-              placeholder='Digite seu usuário'
-              value={ username }
-              onChange={ event => setUsername(event.target.value) }
-              id="username"
-            />
-          </label>
-          <label htmlFor="password" className='text-white flex flex-col w-full'>
+        <label htmlFor="username" className='text-white flex flex-col w-full relative'>
+          Usuário
+          <input
+            type="text"
+            className='p-2 pl-9 rounded-md placeholder-gray-900 placeholder:opacity-60 text-black'
+            name="username"
+            required
+            placeholder='Digite seu usuário'
+            value={ username }
+            onChange={ event => setUsername(event.target.value) }
+            id="username"
+          />
+          <User className='absolute left-2 top-9 text-black/70' />
+        </label>
+          
+          <label htmlFor="password" className='text-white flex flex-col w-full relative'>
             Senha
             <input
-              type="password"
+               type={visibledPassword ? 'text' : 'password'}
               name="password"
               required
               placeholder='Digite sua senha'
-              className='p-2 rounded-md placeholder-gray-900 placeholder:opacity-60 text-black'
+              className='p-2 pl-9 rounded-md placeholder-gray-900 placeholder:opacity-60 text-black'
               value={ password }
               onChange={ event => setPassword(event.target.value) }
               id="password"
             />
-            { isInvalidPassword && <p className='text-red-500 text-sm pt-2'>A sua senha deve conter pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula e um número.</p> }
+            <Lock size={20} weight='thin' className='absolute left-2 top-9 text-black/70' />
+            {visibledPassword ? (
+              <EyeSlash weight='thin' size={20} className='absolute right-2 top-9 text-black/70 cursor-pointer' onClick={() => setVisibledPassword(false)} />
+            ) : (
+              <Eye weight='thin' size={20} className='absolute right-2 top-9 text-black/70 cursor-pointer' onClick={() => setVisibledPassword(true)} />
+            )}
+            { isInvalidPassword && (
+              <p className='text-red-500 text-sm pt-2'>A senha deve conter pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula e um número.</p>
+            )}
           </label>
           { isInvalidUsername && <p className='text-red-500 text-sm pt-2'>Usuário já cadastrado.</p> }
           <button type="submit" className='bg-zinc-600 hover:bg-zinc-700 transition-colors py-2 rounded-md font-medium'>Criar uma conta</button>
