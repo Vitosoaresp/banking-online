b0936c3d14394ea5e809488c5e8d81acfb88b998
Vitor Soares <pereiravitor1218@gmail.com>
2022-11-17 01:00:55 -0300

    feat: rota para a pag de register

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,9 +1,13 @@
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { Login } from './pages/Login';
+import { Register } from './pages/Register';
 
 export function Routes() {
   return (
     <Switch>
+      <Route path="/register">
+        <Register />
+      </Route>
       <Route path="/login">
         <Login />
       </Route>
