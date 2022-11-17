4dffa87370d62cc71c58a3da96869371c99c5f42
Vitor Soares <pereiravitor1218@gmail.com>
2022-11-17 20:32:58 -0300

    fix[service]: remove include de getCashOut

--- server/src/service/transactions/getCashOut.ts
+++ server/src/service/transactions/getCashOut.ts
@@ -4,10 +4,7 @@ export async function getCashOut(accountId:number) {
   const historyCashOut = await prisma.transactions.findMany({
     where: {
       debitedAccountId: accountId,
-    },
-    include: {
-      Accounts: true,
-    },
+    }
   });
   return { code: 200, data: historyCashOut };
 }
