9de5a90fce4b933a1bb531a807d3ae66b8a37f2f
Vitor Soares <pereiravitor1218@gmail.com>
2022-11-18 15:47:49 -0300

    styles[Header]: muda a propriedade weigth dos icones

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -3,8 +3,8 @@ import { Bell, UserCircle } from 'phosphor-react';
 export function Header() {
   return (
     <header className='w-full flex justify-end items-center bg-zinc-900 py-4 px-14 gap-10'>
-      <Bell size={30} weight="regular" color="white" />
-      <UserCircle size={30} weight="regular" color="white" />
+      <Bell size={30} weight="fill" color="white" />
+      <UserCircle size={30} weight="fill" color="white" />
     </header>
   )
 }
