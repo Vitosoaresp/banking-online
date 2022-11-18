72f89f71d16320e583d14e8ac0e1a2addab44206
Vitor Soares <pereiravitor1218@gmail.com>
2022-11-17 21:28:40 -0300

    feat: fetch para pegar o saldo do user

---
+++ web/src/services/fetchBalance.ts
@@ -0,0 +1,11 @@
+import axios, { AxiosResponse } from 'axios';
+import { IBalance } from '../interfaces/IBalance';
+
+export default async function fetchBalance(token: string) {
+  const { data } = await axios.get<unknown, AxiosResponse<IBalance>>('http://localhost:3001/balance', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return data;
+};
