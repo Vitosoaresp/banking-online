16303fdb751c721d20bb0523d19ab04ba530ff9a
Vitor Soares <pereiravitor1218@gmail.com>
2022-11-17 23:18:38 -0300

    styles[AsideMenu]: muda cor de hover dos btn

--- web/src/components/AsideMenu.tsx
+++ web/src/components/AsideMenu.tsx
@@ -13,15 +13,15 @@ export function AsideMenu() {
     <aside className="w-[300px] h-screen px-12 bg-zinc-900 text-white hidden md:flex flex-col justify-between pb-24">
       <div className="flex flex-col gap-8 py-5">
         <Link
-          className="bg-black/60 hover:bg-black/70 rounded-md py-2 px-6 flex justify-start gap-5 items-center"
+          className={`bg-black/60 rounded-md py-2 px-6 flex justify-start gap-5 items-center ${history.location.pathname === '/home' ? 'text-zinc-900 bg-zinc-200' : 'hover:text-zinc-900 hover:bg-zinc-200'} transition-colors duration-500 ease-in-out`}
           to="/home"
         >
           <House size={20} />
           Home
         </Link>
         <Link
-          className="bg-black/60 hover:bg-black/70 rounded-md py-2 px-6 flex justify-start gap-5 items-center"
-          to="/home/transferencias"
+          className={`bg-black/60 rounded-md py-2 px-6 flex justify-start gap-5 items-center ${history.location.pathname === '/transferencias' ? 'text-zinc-900 bg-zinc-200' : 'hover:text-zinc-900 hover:bg-zinc-200'} transition-colors duration-500 ease-in-out`}
+          to="/transferencias"
         >
           <Receipt size={20} />
           Transferencais
@@ -40,7 +40,7 @@ export function AsideMenu() {
         <button
           type="button"
           onClick={handleLogout}
-          className="bg-black/60 hover:bg-black/70 py-2 rounded-md flex justify-start gap-7 items-center px-6 text-zinc-200">
+          className="bg-black/60 hover:bg-zinc-200 hover:text-zinc-900 transition-colors py-2 rounded-md flex justify-start gap-7 items-center px-6 text-zinc-200 duration-500 ease-in-out">
           <SignOut size={20} />
           Sair
         </button>
