c13e3d25bcf15d0c27727ef1872d7b7500c80e66
Vitor Soares <pereiravitor1218@gmail.com>
2022-11-17 23:19:36 -0300

    feat[Home]: mostra saldo e username na tela

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,23 +1,39 @@
+import { useContext } from 'react';
 import { AsideMenu } from '../components/AsideMenu';
 import { Header } from '../components/Header';
+import { bankingContext } from '../context/BankingContext';
+import { IUserLogin } from '../interfaces/IUser';
 
 export function Home() {
+  const { balance } = useContext(bankingContext);
+
+  const userData = JSON.parse(
+    localStorage.getItem('user') as string,
+  ) as IUserLogin;
+
   return (
     <>
       <Header />
-      <main className="text-white w-full flex">
+      <main className='text-white w-full flex'>
         <AsideMenu />
-        <div className="flex flex-col px-20">
-          <div className="w-[450px] bg-zinc-900 py-14 px-20 rounded-md mt-10">
-            <p>
-              Olá, Vitor
+        <div className='flex flex-col px-20'>
+          <div className='w-[450px] bg-zinc-300 py-14 px-20 rounded-md mt-10 gap-5 flex flex-col'>
+            <p className='font-semibold text-2xl text-black'>
+              Olá, {userData && userData.username}
             </p>
-            <p>
-              R$ 3.250,00
-            </p>
+            <div className='flex flex-col'>
+              <p className='text-zinc-800 opacity-80 text-sm'>
+                Seu saldo atual é
+              </p>
+              <span className='text-2xl text-black font-semibold'>
+                {balance === null
+                  ? 'Não foi possível carregar seu saldo'
+                  : `R$ ${(balance / 100).toFixed(2).split('.').join(',')}`}
+              </span>
+            </div>
           </div>
         </div>
       </main>
     </>
-  )
+  );
 }
