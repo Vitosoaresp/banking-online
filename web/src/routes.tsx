baf493cf9d7ff30f00211d8d6275ef63b7d5fc2e
Vitor Soares <pereiravitor1218@gmail.com>
2022-11-17 21:29:30 -0300

    feat[Routes]: nova rota /home

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,10 +1,15 @@
 import { Redirect, Route, Switch } from 'react-router-dom';
+
+import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 
 export function Routes() {
   return (
     <Switch>
+      <Route path="/home">
+        <Home />
+      </Route>
       <Route path="/register">
         <Register />
       </Route>
