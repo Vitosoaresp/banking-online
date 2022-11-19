f2d73687f8dce7809868e73cb75b84c112191713
Vitor Soares <pereiravitor1218@gmail.com>
2022-11-18 23:12:47 -0300

    feat: função q busc as tranferencias á api

--- web/src/services/fetchTransfer.ts
+++ web/src/services/fetchTransfer.ts
@@ -1,12 +1,22 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
-import { ICreateTransfer } from '../interfaces/ITransfer';
+import { ICreateTransfer, ITransfer } from '../interfaces/ITransfer';
 
 export async function fetchCreateTransfer({ accountIn, value, token }: ICreateTransfer & { token: string }) {
   const { data } = await axios.post('http://localhost:3001/transfer', { accountIn, value }, {
     headers: {
       Authorization: token,
     },
+  });
+  return data;
+}
+
+export async function fetchGetTransfers(token: string) {
+  const { data } = await axios
+    .get<unknown, AxiosResponse<ITransfer[]>>('http://localhost:3001/transfer/transfer', {
+      headers: {
+        Authorization: token,
+      },
   });
   return data;
 }
