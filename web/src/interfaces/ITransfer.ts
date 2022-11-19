101fe98b501326270f7f828f07efe968e28c1758
Vitor Soares <pereiravitor1218@gmail.com>
2022-11-18 23:13:06 -0300

    feat[Interfaces]: ITransfer

--- web/src/interfaces/ITransfer.ts
+++ web/src/interfaces/ITransfer.ts
@@ -2,3 +2,11 @@ export interface  ICreateTransfer {
   accountIn: number;
   value: number;
 }
+
+export interface ITransfer {
+  id: string;
+  value: number;
+  debitedAccountId: number;
+  creditedAccountId: number;
+  createdAt: string;
+}
