2613a3ef51e4d01b3830355f621ec523109dadbe
Vitor Soares <pereiravitor1218@gmail.com>
2022-11-21 18:06:18 -0300

    feat[header]: logout pelo header ao clicar no icone User

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,10 +1,42 @@
+import { Popover } from '@headlessui/react'
 import { Bell, UserCircle } from 'phosphor-react'
+import { useHistory } from 'react-router-dom'
 
 export function Header() {
+  const history = useHistory()
+
+  function handleLogout() {
+    localStorage.clear()
+    history.push('/login')
+  }
+
   return (
     <header className="w-full flex justify-end items-center bg-zinc-900 py-4 px-14 gap-10">
       <Bell size={30} weight="fill" color="white" />
-      <UserCircle size={30} weight="fill" color="white" />
+      <Popover>
+        <Popover.Button>
+          <UserCircle size={30} weight="fill" color="white" />
+        </Popover.Button>
+
+        <Popover.Panel className="absolute right-0 w-[250px] bg-zinc-900 border border-zinc-800 rounded-md shadow-lg flex flex-col py-4 px-8 z-20">
+          <div className="flex justify-center text-white font-semibold text-lg">
+            Editar Perfil
+          </div>
+          <div className="py-2">
+            <p className="flex cursor-not-allowed gap-1 text-md items-center">
+              Editar Imagem{' '}
+              <span className="text-xs text-red-600 text-right">Em breve</span>
+            </p>
+          </div>
+          <hr className="w-full py-2" />
+          <button
+            className="py-2 px-4 rounded-md bg-zinc-200 hover:bg-zinc-300 transition-colors text-zinc-800 font-bold"
+            onClick={handleLogout}
+          >
+            Sair
+          </button>
+        </Popover.Panel>
+      </Popover>
     </header>
   )
 }
