75ae441cff463099823e3416fe341cd189582787
Vitor Soares <pereiravitor1218@gmail.com>
2022-11-21 15:25:18 -0300

    feat: script db push em package

--- server/src/service/users/createUser.ts
+++ server/src/service/users/createUser.ts
@@ -3,13 +3,13 @@ import prisma from '../../client';
 
 export async function createUsers(username: string, password: string) {
   if (!username || !password) {
-    return { code: 400, message: 'usuário ou senha em branco' };
+    return { code: 400, message: 'Usuário ou senha em branco' };
   }
   if (username.length < 3) {
-    return { code: 400, message: 'usuário deve conter ao menos 3 caracteres' };
+    return { code: 400, message: 'Usuário deve conter ao menos 3 caracteres' };
   }
   if (password.length < 8) {
-    return { code: 400, message: 'senha deve conter ao menos 8 caracteres' };
+    return { code: 400, message: 'A senha deve conter ao menos 8 caracteres' };
   }
   const existingUser = await prisma.users.findUnique({
     where: {
@@ -17,7 +17,7 @@ export async function createUsers(username: string, password: string) {
     },
   });
   if (existingUser) {
-    return { code: 400, message: 'usuário já existe' };
+    return { code: 400, message: 'Usuário já existe' };
   }
   const passHash = crypto.createHash('md5').update(password).digest('hex');
   await prisma.users.create({
@@ -31,5 +31,5 @@ export async function createUsers(username: string, password: string) {
       }
     },
   });
-  return { code: 201, message: 'usuário criado com sucesso' };
+  return { code: 201, message: 'Usuário criado com sucesso' };
 }
