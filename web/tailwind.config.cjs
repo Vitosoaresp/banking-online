42faabc7e1a974fa6bc28c5a48f038aad3cd5b9f
Vitor Soares <pereiravitor1218@gmail.com>
2022-12-01 16:51:13 -0300

    chore: move sgv para pasta public

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,17 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        roboto: ["Roboto", "sans-serif"],
+        roboto: ['Roboto', 'sans-serif'],
       },
       backgroundImage: {
-        "bg-animation": "url('./src/assets/bg-animation.svg')",
-        "login-animation": "url('./src/assets/bg-animation2.svg')",
-      }
+        'bg-animation': "url('/bg-animation.svg')",
+        'login-animation': "url('/bg-animation2.svg')",
+      },
     },
   },
   plugins: [],
